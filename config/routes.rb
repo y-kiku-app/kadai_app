f3341dd3d9dd351ecd5614c218ae937168c75f14
yuto.kikuchi <yuto.kikuchi@appirits-1010696noMacBook-Pro.local>
2021-08-13 14:42:17 +0900

    #1 ログイン確認のためマイページを簡易作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
-  # root 'users#index'
-
+  get 'mypage/show'
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy"
     post 'users/sign_up/confirm', to: 'users/registrations#confirm'
     get 'users/sign_up/complete', to: 'users/registrations#complete'
   end
+
+  resources :mypage, only: [:show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
