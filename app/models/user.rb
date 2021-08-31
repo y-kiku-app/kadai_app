b8488009f2c15d6caba5537f2f4732729fea2842
yuto.kikuchi <yuto.kikuchi@appirits-1010696noMacBook-Pro.local>
2021-08-31 18:47:12 +0900

    情報変更の際に現在のパスワード要求を排除

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,18 @@ class User < ApplicationRecord
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
+
+  def update_with_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank?
+        params.delete(:password)
+        params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update(params, *options)
+
+    clean_up_passwords
+    result
+  end
 end
